42711c82b2a50cd673a99bb24e4ff0b67cf97526
Oleg Olanovski <oleg-olanovski@t-online.de>
2026-04-01 11:58:10 +0200

    add cloud animation

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -7,5 +7,12 @@ class Cloud extends MovableObject {
     super().loadimage("img/5_background/layers/4_clouds/1.png");
 
     this.x = Math.random() * 500; // Random y position for the cloud
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.x -= 0.15; // Move the cloud to the left
+    }, 1000 / 60); // 60 frames per second
   }
 }
