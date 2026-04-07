81c40759be075161345e1b257eee8bedc288c035
Oleg Olanovski <oleg-olanovski@t-online.de>
2026-04-07 09:18:17 +0200

    add character jump

--- models/character.class.js
+++ models/character.class.js
@@ -11,11 +11,24 @@ class Character extends MovableObject {
     "img/2_character_pepe/2_walk/W-25.png",
     "img/2_character_pepe/2_walk/W-26.png",
   ];
+
+  IMAGES_JUMPING = [
+    "img/2_character_pepe/3_jump/J-31.png",
+    "img/2_character_pepe/3_jump/J-32.png",
+    "img/2_character_pepe/3_jump/J-33.png",
+    "img/2_character_pepe/3_jump/J-34.png",
+    "img/2_character_pepe/3_jump/J-35.png",
+    "img/2_character_pepe/3_jump/J-36.png",
+    "img/2_character_pepe/3_jump/J-37.png", 
+    "img/2_character_pepe/3_jump/J-38.png",
+    "img/2_character_pepe/3_jump/J-39.png",
+  ];
   world;
 
   constructor() {
     super().loadimage("img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_JUMPING);
     this.applyGravity();
     this.animate();
   }
@@ -31,14 +44,23 @@ class Character extends MovableObject {
         this.x -= this.speed; // Move to the left
         this.otherDirection = true;
       }
+      if (this.world.keyboard.UP) {
+        this.speedY = 30; // Jumping speed
+      }
       this.world.camera_x = -this.x + 100; // Kamera folgt dem Charakter
     }, 1000 / 60); // 60 frames per second
 
     setInterval(() => {
-      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-        // Walking animation
-        this.playAnimation(this.IMAGES_WALKING);
-      }
+if (this.isAboveGround()) {
+        // Jumping animation
+        this.playAnimation(this.IMAGES_JUMPING);
+      } else {
+
+             if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+             // Walking animation
+             this.playAnimation(this.IMAGES_WALKING);
+              }
+            }
     }, 50);
   }
   jump() {}
